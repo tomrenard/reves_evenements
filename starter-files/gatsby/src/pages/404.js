0e5ae7805ed2e402c869f684972d1f647525ee24
Tom Renard <tom.renard@zoho.com>
2021-05-03 17:15:32 +0200

    set up gatsby + sanity

---
+++ starter-files/gatsby/src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Four0Four() {
+  return (
+    <p>That page doesn't exist.</p>
+  );
+}
---
+++ starter-files/sanity/schemas/event.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'event',
+  title: 'Events',
+  type: 'document',
+  fields: [
+    {
+      name: 'type',
+      title: 'Event type',
+      type: 'string',
+    }
+  ]
+};
