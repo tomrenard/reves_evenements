eb621e0987da65ed40439b2b104814ec92943efa
Tom Renard <tom.renard@zoho.com>
2021-05-20 18:23:25 +0200

    event gammes wip

--- starter-files/gatsby/src/components/EventsGammes.js
+++ starter-files/gatsby/src/components/EventsGammes.js
@@ -3,29 +3,46 @@ import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
 
+const ColumsGammesStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
 export default function EventsGammes() {
   const data = useStaticQuery(graphql`query Gammes {
     allSanityHomePage {
       nodes {
-        title2
-        event1
-        event2
-        event3
-        imagelastevent1 {
+        title3
+        typeevent1
+        typeevent2
+        typeevent3
+        typeevent4
+        contentevent1
+        contentevent2
+        contentevent3
+        contentevent4
+        imagetypeevent1 {
           asset {
             fluid(maxWidth: 800) {
               ...GatsbySanityImageFluid
             }
           }
         }
-        imagelastevent2 {
+        imagetypeevent2 {
           asset {
             fluid(maxWidth: 800) {
               ...GatsbySanityImageFluid
             }
           }
         }
-        imagelastevent3 {
+        imagetypeevent3 {
+          asset {
+            fluid(maxWidth: 800) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        imagetypeevent4 {
           asset {
             fluid(maxWidth: 800) {
               ...GatsbySanityImageFluid
@@ -37,7 +54,28 @@ export default function EventsGammes() {
   }
 `)
   const content = data.allSanityHomePage.nodes[0];
+  console.log(content);
   return (
-  <p>Events gammes</p>
+    <>
+      <h2>{content.title3}</h2>
+      <ColumsGammesStyles>
+        <div className="column1">
+          <h3>{content.typeevent1}</h3>
+          <p>{content.contentevent1}</p>
+        </div>
+        <div className="column2">
+          <h3>{content.typeevent2}</h3>
+          <p>{content.contentevent2}</p>
+        </div>
+        <div className="column3">
+          <h3>{content.typeevent3}</h3>
+          <p>{content.contentevent3}</p>
+        </div>
+        <div className="column4">
+          <h3>{content.typeevent4}</h3>
+          <p>{content.contentevent4}</p>
+        </div>
+      </ColumsGammesStyles>
+    </>
   );
 }
