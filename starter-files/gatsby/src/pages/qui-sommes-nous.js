fce85e587928ec135384bb75de7ef36ae5fad2ae
Tom Renard <tom.renard@zoho.com>
2021-06-10 15:11:16 +0200

    data to component about us

--- starter-files/gatsby/src/pages/qui-sommes-nous.js
+++ starter-files/gatsby/src/pages/qui-sommes-nous.js
@@ -4,16 +4,63 @@ import HistoryAbout from '../components/HistoryAbout';
 import ArticleAbout from '../components/ArticleAbout';
 import ValuesAbout from '../components/ValuesAbout';
 import ContactForm from '../components/ContactForm';
+import { graphql } from 'gatsby';
 
 
-export default function About() {
+export default function About({ data }) {
+  const aboutUs = data.aboutus.nodes[0];
+  const values = data.values.nodes;
   return (
     <>
-      <HeaderAbout />
-      <HistoryAbout />
-      <ArticleAbout />
-      <ValuesAbout />
+      <HeaderAbout aboutUs={aboutUs} />
+      <HistoryAbout aboutUs={aboutUs} />
+      <ArticleAbout aboutUs={aboutUs} />
+      <ValuesAbout values={values} />
       <ContactForm />
     </>
   );
 }
+
+
+export const query = graphql`
+  query {
+    values: allSanityValue {
+      nodes {
+        _id
+        titlevalue
+        contentvalue
+        imagebloc2 {
+          asset {
+            fluid(maxWidth: 800) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+    aboutus: allSanityAboutUs {
+      nodes {
+        title1
+        titlebloc2
+        title3
+        content1
+        content2
+        content3
+        imagebloc2 {
+          asset {
+            fluid(maxWidth: 800) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        imagebloc3 {
+          asset {
+            fluid(maxWidth: 800) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
